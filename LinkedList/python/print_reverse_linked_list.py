227848a46130ce9292048cc4620ff18ceb928be2
Md.Abdullah al sadid <abdullahalsadid1914@gmail.com>
2025-05-20 16:33:34 +0600

    print linked_list reverse

---
+++ LinkedList/python/print_reverse_linked_list.py
@@ -0,0 +1,45 @@
+import sys
+class Node:
+    value = None
+    next = None
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+
+
+def insert_at_tail(head_container,value):
+    temp = head_container["head"]
+    newNode = Node(value)
+    if temp == None:
+        head_container["head"] = newNode
+        return 
+
+    while temp.next is not None:
+        temp = temp.next
+    
+    temp.next = newNode
+
+def print_node_reverse(head):
+    temp = head
+    if temp == None:
+        return 
+    print_node_reverse(temp.next)
+    print(temp.value,end=" ")
+
+head_container = {"head":None}
+input_data = sys.stdin.read().strip().split()
+a = list(map(int,input_data))
+print(a)
+i=0
+while True:
+    if a[i] != -1:
+        insert_at_tail(head_container,a[i])
+    else :
+        break
+    i+=1
+print_node_reverse(head_container["head"])
+
+
+    
+
---
+++ LinkedList/typescript/print_reverse_linked_list.ts
@@ -0,0 +1,61 @@
+import * as readline from 'readline'
+
+const rl = readline.createInterface({
+    input:process.stdin
+})
+
+let input_data:string=""
+
+rl.on('line',(line)=>{
+    input_data+=line
+})
+
+class Node<T>{
+    value:T;
+    next:Node<T> | null
+
+    constructor(value:T){
+        this.value = value,
+        this.next = null
+    }
+}
+
+function insert_at_tail(head_container:{head:Node<number> | null},value:number){
+    const newNode = new Node(value)
+    let temp:Node<number> | null = head_container["head"]
+    if (temp === null){
+        head_container["head"] = newNode
+        return 
+    }
+    while (temp.next !=null ){
+        temp=temp?.next
+    }
+    temp.next = newNode
+}
+
+function print_node_reverse(head:Node<number>|null){
+    let temp:Node<number> | null = head
+    if (temp === null){
+        return 
+    }
+    print_node_reverse(temp.next)
+    process.stdout.write(temp.value + " ")
+}
+
+let head_container = {"head":null}
+
+rl.on('close',()=>{
+    const a = input_data.split(" ").map(Number)
+    let i = 0
+    while (true){
+        if(a[i]!=-1){
+            insert_at_tail(head_container,a[i])
+        }else{
+            break
+        }
+        i++
+    }
+    print_node_reverse(head_container["head"])
+    console.log()
+})
+
